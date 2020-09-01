34556566589b93f1737823af613978172011fe92
snow <sergiossc@gmail.com>
2020-09-01 15:12:11 -0300

    not used functions commented

--- utils.py
+++ utils.py
@@ -14,28 +14,6 @@ def squared_norm(cw):
 def norm(cw):
     return np.sqrt(squared_norm(cw))
 
-def std_deviation(vector):
-    de_meaned = vector - average(vector)
-    return norm(de_meaned) * 1/np.sqrt(len(vector))
-
-def rms():
-    return np.sqrt(np.power(average(x), 2) + np.power(std_deviation(x), 2))
-
-def complex_correlation(cw1, cw2):
-    cw1 = cw1/norm(cw1)
-    cw2 = cw2/norm(cw2)
-    u = np.matrix([np.real(cw1), np.imag(cw1)])
-    u_vec = np.array(u).reshape(np.size(u))
-    v = np.matrix([np.real(cw2), np.imag(cw2)])
-    v_vec = np.array(v).reshape(np.size(v))
-    correlation = np.inner(u_vec, v_vec)
-    return correlation
-
-def correlation_factor(x, y):
-    de_meaned_x = x - average(x)
-    de_meaned_y = y - average(y)
-    return np.inner(de_meaned_x, de_meaned_y) / (norm(de_meaned_x) * norm(de_meaned_y))
-
 def gen_dftcodebook(num_of_cw):
     tx_array = np.arange(num_of_cw)
     mat = np.matrix(tx_array).T * tx_array
@@ -43,11 +21,9 @@ def gen_dftcodebook(num_of_cw):
     return cb
     
 def gen_samples(codebook, num_of_samples, variance, same_samples_for_all=True):
-
     if same_samples_for_all:
         samples_seed = 789
         np.random.seed(samples_seed)
-
     num_rows = np.shape(codebook)[0]
     num_coluns = np.shape(codebook)[1]
     samples = []
@@ -56,9 +32,7 @@ def gen_samples(codebook, num_of_samples, variance, same_samples_for_all=True):
             noise = np.sqrt(variance/2) * (np.random.randn(1, num_coluns) + np.random.randn(1, num_coluns) * 1j)
             sample = cw + noise
             samples.append(sample)
-
     np.random.seed(None)
-
     return np.array(samples)
 
 def complex_average(samples):
@@ -72,26 +46,6 @@ def duplicate_codebook(codebook, perturbation_vector):
         new_codebook.append(cw1)
         new_codebook.append(cw2)
     return np.array(new_codebook)
-
-def get_mean_distortion(sets, samples, codebook):
-    sum_squared_error = 0
-    for cw_id, samples_id_list in sets.items():
-        cw = codebook[cw_id]
-        for sample_id in samples_id_list:
-            sample = samples[sample_id]    
-            squared_error = np.sum(complex_squared_error(cw, sample))
-            sum_squared_error += squared_error
-    return sum_squared_error/len(samples)
-
-def plot_performance(distortion_by_round, graph_title, filename):
-    fig, ax = plt.subplots()
-    for r, mean_distortion in distortion_by_round.items():
-        ax.plot(mean_distortion, label='#cw: ' + str(2**r))
-    plt.ylabel('distortion (MSE)')
-    plt.xlabel('# iterations')
-    plt.title(graph_title)
-    plt.legend()
-    fig.savefig(filename)
 
 def dict2matrix(dict_info):
     vector = []
@@ -120,26 +74,20 @@ def gain_distortion(sample, codebook_dict):
     max_gain = -np.Inf
     max_cw_id = None
     for cw_id, cw in codebook_dict.items():
-        gain = np.inner(sample.conj(), cw)
+        gain = np.real(np.inner(sample.conj(), cw))
         #gain = np.abs(np.inner(sample, cw))
         if gain > max_gain:
             max_gain = gain
             max_cw_id = cw_id
     return max_cw_id, max_gain
- 
 
 def perform_distortion(sample, codebook_dict, metric):
-
     cw_id = None
     distortion = None
-
     distortion_opts = {'mse': mse_distortion, 'gain': gain_distortion}
     distortion_function = distortion_opts.get(metric, None)
-
     cw_id, distortion = distortion_function(sample, codebook_dict)
-
     return cw_id, distortion
-
 
 def lloyd_gla(initial_alphabet_opt, samples, num_of_levels, num_of_iteractions, distortion_measure, perturbation_variance=None, initial_codebook=None):
     """
@@ -174,22 +122,18 @@ def lloyd_gla(initial_alphabet_opt, samples, num_of_levels, num_of_iteractions, 
 
 
     for r in range(1, num_of_rounds+1):
-
-
         if initial_alphabet_opt == 'unitary_until_num_of_elements':
             codebook = duplicate_codebook(codebook, perturbation_vector)
         elif initial_alphabet_opt == 'random_from_samples':
             pass
         else:
             return None
-
         samples_dict = matrix2dict(samples)
         mean_distortion_by_iteractions = np.zeros(num_of_iteractions)
 
         for n in range(num_of_iteractions):
-
             codebook_dict = matrix2dict(codebook)
-            
+
             sets = {}  # Storage information of partitions baised by each codewords
             for cw_id in codebook_dict.keys():
                 sets[cw_id] = []
@@ -227,6 +171,7 @@ def lloyd_gla(initial_alphabet_opt, samples, num_of_levels, num_of_iteractions, 
     return dict2matrix(current_codebook_dict), sets,  mean_distortion_by_round
 
 
+# JSON STUFF TO ENCODE/DECODE DATA
 def encode_codebook(codebook):
     codebook_enc = {}
     for cw_id, cw in codebook.items():
@@ -248,16 +193,13 @@ def encode_sets(sets):
 
 def encode_mean_distortion(distortion_by_round):
     distortion_enc = {}
-
     for r, distortion_by_interactions in distortion_by_round.items():
         count = 0
         distortion_by_interactions_enc = {}
         for d in distortion_by_interactions:
             distortion_by_interactions_enc[str(count)] = float(d)
             count += 1
-
         distortion_enc[str(r)] = distortion_by_interactions_enc
-    
     return distortion_enc
 
 def decode_codebook(codebook_json):
@@ -267,17 +209,57 @@ def decode_codebook(codebook_json):
         for cw_adjust in cw.items():
             real_adjust = cw_adjust[1][0]
             imag_adjust = cw_adjust[1][1]
-
             adjust = real_adjust + 1j * imag_adjust
             codeword.append(adjust)
-
         codebook_dict[cw_id] = np.array(codeword, dtype=complex)
-
     return codebook_dict
 
-def save_training_samples(samples):
-    np.save('samples.npy', samples)
+#def save_training_samples(samples):
+#    np.save('samples.npy', samples)
 
-def load_samples(filename):
-    samples = np.load(filename)
-    return samples
+#def load_samples(filename):
+#    samples = np.load(filename)
+#    return samples
+
+
+#def std_deviation(vector):
+#    de_meaned = vector - average(vector)
+#    return norm(de_meaned) * 1/np.sqrt(len(vector))
+
+#def rms():
+#    return np.sqrt(np.power(average(x), 2) + np.power(std_deviation(x), 2))
+
+#def complex_correlation(cw1, cw2):
+#    cw1 = cw1/norm(cw1)
+#    cw2 = cw2/norm(cw2)
+#    u = np.matrix([np.real(cw1), np.imag(cw1)])
+#    u_vec = np.array(u).reshape(np.size(u))
+#    v = np.matrix([np.real(cw2), np.imag(cw2)])
+#    v_vec = np.array(v).reshape(np.size(v))
+#    correlation = np.inner(u_vec, v_vec)
+#    return correlation
+
+#def correlation_factor(x, y):
+#    de_meaned_x = x - average(x)
+#    de_meaned_y = y - average(y)
+#    return np.inner(de_meaned_x, de_meaned_y) / (norm(de_meaned_x) * norm(de_meaned_y))
+
+#def get_mean_distortion(sets, samples, codebook):
+#    sum_squared_error = 0
+#    for cw_id, samples_id_list in sets.items():
+#        cw = codebook[cw_id]
+#        for sample_id in samples_id_list:
+#            sample = samples[sample_id]    
+#            squared_error = np.sum(complex_squared_error(cw, sample))
+#            sum_squared_error += squared_error
+#    return sum_squared_error/len(samples)
+
+#def plot_performance(distortion_by_round, graph_title, filename):
+#    fig, ax = plt.subplots()
+#    for r, mean_distortion in distortion_by_round.items():
+#        ax.plot(mean_distortion, label='#cw: ' + str(2**r))
+#    plt.ylabel('distortion (MSE)')
+#    plt.xlabel('# iterations')
+#    plt.title(graph_title)
+#    plt.legend()
+#    fig.savefig(filename)
